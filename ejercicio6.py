d2283ab5cafbad25254ecc56ca09ee5396a006cf
alcivar-inca <eddy_epn2008@hotmail.com>
2016-06-09 09:51:47 -0500

    ejercicio 6
    Rombo

--- ejercicio6.py
+++ ejercicio6.py
@@ -4,16 +4,18 @@ def triangulos():
 	x1=0; y1=30
 	x2=20 ; y2=0
 	x3=40
+	y4=60
 
 	ancho= 0
 	alto=0
 	ventana= Tk() # Creamos un objeto Tk
 	ventana.title("Ejercicio 6") # Titulo de la ventana
-	canvas = Canvas(ventana, width=400, height=400)
+	canvas = Canvas(ventana, width=400, height=300)
 	canvas.pack()
-	while alto<=360:
+	while alto<=270:
 		while ancho <=360:
-			canvas.create_polygon(x1,y1 ,x2,y2 ,x3,y1,fill= 'black')
-			ventana.mainloop()
+			canvas.create_polygon(x1,y1 ,x2,y2 ,x3,y1, x2,y4, fill= 'black')
+			
+		ventana.mainloop()
 
 triangulos()
