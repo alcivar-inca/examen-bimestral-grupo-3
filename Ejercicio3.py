c097545631813a569c63ef3e0368f4112ba9d2bf
santiagotoapanta6 <santymaster3@gmail.com>
2016-06-09 09:13:57 -0500

    ejercicio3_2
    falta guardar en archivo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,19 +1,21 @@
 
+
 def archivo():
 
 	archi=open('texto.txt','r')
 	texto=archi.readline()
 	contador=0
+	palab=0
 	while texto!="":
 		cont=texto.split(".")
 		for i in range(len(cont)):
 			palabras=len(cont[i].split(" "))
 			contador=contador+1
-			print (str(palabras) +" palabras")
-			print (str(contador))
+			palab=palab+palabras
 			texto=archi.readline()
+	print ("Existen "+str(contador+1)+" saltos de linea")
+	print ("Existen "+str(palab) +" palabras")
 
 
 archivo()
 	
-
